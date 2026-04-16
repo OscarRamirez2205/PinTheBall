f4deceefa73dcc41114c7fc460823b7575f514d1
Oscar Ramirez Sanchez <oscar.ram.san2205@gmail.com>
2026-04-16 18:49:33 +0200

    (Front)Feat: Definidos los colores de la aplicacion en el homepage

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,8 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    redirectTo: 'login',
+    loadComponent: () => import('./homepage/homepage').then((m) => m.Homepage),
   },
   {
     path: 'login',
