5b6a89228fd2825accfbf7d5f734a0902b07af80
Oscar Ramirez Sanchez <oscar.ram.san2205@gmail.com>
2026-06-03 12:35:28 +0200

    (Front)Fix: Ajuste de la camara para que este mejor

--- frontend/src/app/game/game.ts
+++ frontend/src/app/game/game.ts
@@ -263,7 +263,7 @@ export class Game implements AfterViewInit, OnDestroy {
           new BABYLON.Vector3(0, 12, -4), 
           scene);
 
-      camera.setTarget(new BABYLON.Vector3(0, 2, 0));
+      camera.setTarget(new BABYLON.Vector3(0, 4, 0));
 
       camera.attachControl(canvas, true);
 
