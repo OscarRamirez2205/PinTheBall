0a7b1492e5764f61df71b2757c09a73eb1052de3
Oscar Ramirez Sanchez <oscar.ram.san2205@gmail.com>
2026-06-04 17:18:47 +0200

    (Front)Fix: Ajuste de velocidad de la camara

--- frontend/src/app/game/game.ts
+++ frontend/src/app/game/game.ts
@@ -264,11 +264,7 @@ export class Game implements AfterViewInit, OnDestroy {
           scene);
 
       camera.setTarget(new BABYLON.Vector3(0, 6, 0));
-
       camera.speed = 0.15;
-      camera.angularSensibility = 5000;
-      camera.wheelPrecision = 60;
-      camera.inertia = 0.94;
 
       camera.attachControl(canvas, true);
 
